bc2a0ec83f621e8d9ed065c6fb77ad0d7d117fcc
Bryan Ortiz <bryan@bryanortiz.com>
2017-12-28 17:13:54 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,3 @@
-=begin
-def reverse_each_word(string)
-  array = string.split(/, /)
-  array.each do |i|
-    array[i].reverse.join
-  end
-
-end
-=end
 
 def reverse_each_word(sentence)
   sentence.split.collect {|word| word.reverse}.join(" ")
