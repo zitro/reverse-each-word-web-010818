9c13101d11cae7004777476dc14f24abbb699f4d
Bryan Ortiz <bryan@bryanortiz.com>
2017-12-28 17:13:45 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,4 @@
 
-def reverse_each_word(string)
-  array = string.split(/, /)
-  array.each do |i|
-    array[i].reverse.join
-  end
-
-end
-
-=begin
 def reverse_each_word(sentence)
   sentence.split.collect {|word| word.reverse}.join(" ")
 end
-=end
